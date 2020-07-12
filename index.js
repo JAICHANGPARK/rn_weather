1b1aa7819dbcd5bcd458befe00d5dc363e2d4f66
Dreamwalker <aristojeff@gmail.com>
2020-07-12 16:24:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import MyApp from './app/ui/Main'
+import MainApp from './app/ui/MainApp'
 
-AppRegistry.registerComponent(appName, () => MyApp);
+AppRegistry.registerComponent(appName, () => MainApp);
